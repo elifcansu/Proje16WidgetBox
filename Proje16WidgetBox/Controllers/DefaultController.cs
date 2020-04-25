9042026997d806e483510f3a9b96c295a949774e
elifcansu.001@outlook.com <elifcansu.001@outlook.com>
2020-04-25 16:30:01 +0300

    Add project files.

---
+++ Proje16WidgetBox/Controllers/DefaultController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Proje16WidgetBox.Models.Entity;
+
+namespace Proje16WidgetBox.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        SchoolDbEntities1 db = new SchoolDbEntities1();
+        public ActionResult Index()
+        {
+            var deger1 = db.TBLOGRENCİ.Count();
+            var deger2 = db.TBLOGRENCİ.Where(x => x.SEHIR == "ankara").Count();
+            ViewBag.dgr1 = deger1;
+            ViewBag.dgr2 = deger2;
+            return View();
+        }
+    }
+}
---
+++ Proje16WidgetBox/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proje16WidgetBox.MvcApplication" Language="C#" %>
